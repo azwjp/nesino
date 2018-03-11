f4964d194b1149f75783b44f325b5fa06ae83a59
Akane Sayama <a@azw.jp>
2018-03-11 11:32:33 +0900

    Change: rewrite comments

--- sketch_feb22a.ino
+++ sketch_feb22a.ino
@@ -5,45 +5,45 @@ volatile byte enableFlag = 0b00001110; // 000 noiseShortFrag sq1 sq2 tri noi
 volatile int sq1Freq = 45; // 0-2047 << 5
 volatile bool sq1Env = false; // Envelope
 volatile byte sq1Duty = 2; // 0-3
-volatile byte sq1FC = 0; // FreqChange 周波数変更量
-volatile bool sq1FCDirection = false; // 周波数変更方向 true->上がっていく
-volatile byte sq1FCCount = 0; // 周波数変更カウント数
-volatile bool sq1Sweep = false; // スイープ有効フラグ
-volatile byte sq1Vol = 6; // 音量 0-15
+volatile byte sq1FC = 0; // FreqChange Freqency change amount
+volatile bool sq1FCDirection = false; // Frequency change direction: true -> up
+volatile byte sq1FCCount = 0; // Freqency change count
+volatile bool sq1Sweep = false; // Flag of sweep enabled
+volatile byte sq1Vol = 6; // Volume 0-15
 // sq1 カウンタ
-volatile byte sq1Pointer = -1; // 波形のどこを再生しているか
-volatile int sq1Counter = 0; // 分周器
+volatile byte sq1Pointer = -1; // Current phase
+volatile int sq1Counter = 0; // divider
 
 // sq2 設定
 volatile int sq2Freq = 20;//85; // 0-2047 << 5
 volatile bool sq2Env = false; // Envelope
 volatile byte sq2Duty = 1; // 0-3
-volatile byte sq2FC = 0; // FreqChange 周波数変更量
-volatile bool sq2FCDirection = false; // 周波数変更方向 true->上がっていく
-volatile byte sq2FCCount = 0; // 周波数変更カウント数
-volatile bool sq2Sweep = false; // スイープ有効フラグ
-volatile byte sq2Vol = 7; // 音量 0-15
+volatile byte sq2FC = 0; // FreqChange Freqency change amount
+volatile bool sq2FCDirection = false; // Frequency change direction: true -> up
+volatile byte sq2FCCount = 0; // Freqency change count
+volatile bool sq2Sweep = false; // Flag of sweep enabled
+volatile byte sq2Vol = 7; // Volume 0-15
 // sq1 カウンタ
-volatile byte sq2Pointer = -1; // 波形のどこを再生しているか
-volatile int sq2Counter = 0; // 分周器
+volatile byte sq2Pointer = -1; // Current phase
+volatile int sq2Counter = 0; // divider
 
 
 // tri 設定
 volatile bool triEnable = false;
 volatile int triFreq = 10;//85; // 0-2047 << 5
 volatile bool triEnv = false; // Envelope
-volatile byte triFC = 0; // FreqChange 周波数変更量
-volatile bool FCDirection = false; // 周波数変更方向 true->上がっていく
-volatile byte triFCCount = 0; // 周波数変更カウント数
-volatile bool triSweep = false; // スイープ有効フラグ
+volatile byte triFC = 0; // FreqChange Freqency change amount
+volatile bool FCDirection = false; // Frequency change direction: true -> up
+volatile byte triFCCount = 0; // Freqency change count
+volatile bool triSweep = false; // Flag of sweep enabled
 // tri カウンタ
-volatile byte triPointer = -1; // 波形のどこを再生しているか
-volatile int triCounter = 0; // 分周器
+volatile byte triPointer = -1; // Current phase
+volatile int triCounter = 0; // divider
 
 // noise
 volatile unsigned int noiseReg = 0x8000;
 volatile int noiseCountMax = 0x20;
-volatile byte noiseVol = 10; // 音量 0-15 * rangeMax / 15
+volatile byte noiseVol = 10; // Volume 0-15
 // noise カウンタ
 volatile int noiseCounter = 0;
 
@@ -52,50 +52,56 @@ volatile byte currentNoise = 0;
 void setup() {
   cli();
 
-  /* TIMER0: 出力の強さを制御するための PWM
-     できるだけ高速で回す
-     Top 255 なので 62500 Hz の PWM
-     OCR0A の値を変えることで制御できる．
+  /* TIMER0: PWM for change the output level
+     As faster as possible.
+     The top is OCR0A, and it is set at 63 because the resolution needs 64.
+     This means 250 kHz PWM.
+     Change the value of OCR0B to change the output signal's level.
+     OCR0B should be 0 to 63.
   */
-  // pin6有効，pin9なし，高速PWM，コンペアマッチでLOW，TOPは0xFF，
+  // PD6 (pin6) invalid, PD5 (pin5) valid. fast PWM. Compare match then HIGH to LOW. Top is OCR0A，
   TCCR0A = 0b10100011;
   TCCR0B |= _BV(WGM02);
-  // 分周 1/1
+  // no division
   TCCR0B = (TCCR0B & 0b11111000) | 0b00000001;
-  // compare register -> 音を鳴らすときに毎回変わる
+  // Top
   OCR0A = 64;
+  // compare register -> output level
   OCR0B = 0;
-  // 割り込み無し
+  // no interrupts
   TIMSK0 = 0;
 
-  /* TIMER2: 波形を変更するタイミング
-     本当は OCR2A = 8.9 にしたい．
-     早すぎるとTIMER1の割り込みに失敗するので 71
+  /* TIMER2: interrupts when change the output level
+     OCR2A should be 8.9 to set the frequency of NES.
+     If too fast, the TIMER1 interrupts will fail.
   */
   // CTC
   TCCR2A = 0b00000010;
   TCCR2B &= ~_BV(WGM02);
-  // 分周 1/1
+  // no devision
   TCCR2B = (TCCR2B & 0b11111000) | 0b00000001;
   // compare register
   OCR2A = 80;
-  // interrupt when TCNT1 == OCR1A
+  // interrupt when TCNT2 == OCR2A
   TIMSK2 = _BV(OCIE2A);
 
-  // TIMER1: 音の出力の設定を変更する
+  /* TIMER1: chenge the parameters of sound
+   * NES is 240 fps
+   */
+  
   // CTC (ICR1)
   TCCR1B = (TCCR1B & ~_BV(WGM13)) | _BV(WGM12);
   TCCR1A = TCCR1A & ~(_BV(WGM11) | _BV(WGM10));
 
   // 1/8
-  TCCR1B = (TCCR1B & ~_BV(CS11)) | _BV(CS12) | _BV(CS10);
+  TCCR1B = (TCCR1B & ~_BV(CS11)) | _BV(CS12) | _BV(CS10); // 1/1024
   // TCCR1B = (TCCR1B & ~(_BV(CS12) | _BV(CS10))) |_BV(CS11);
 
   // compare register
-  // 16 MHz / 割り込み周波数 (Hz)
-  OCR1A = 8332;//8332;
+  // 16 MHz / frequency of interrupt (Hz)
+  OCR1A = 8332;
 
-  // 割り込み
+  // interrupt when TCNT1 == OCR1A
   TIMSK1 = _BV(OCIE1A);
 
   pinMode(13, OUTPUT);
@@ -123,12 +129,12 @@ void setup() {
   "STARTLOOP: "
     "sbrs r20, 0 \n"
     "rjmp STARTLOOP \n"
-    
+
     "clr r2 \n"
 
 // if noise is enabled {
     "lds r3, enableFlag \n"
-    // noiseがenableでなければ分岐
+    // if noise isn't enable, then branch
     "sbrs r3, 0 \n"
     "rjmp SQ1 \n"
 
@@ -152,10 +158,10 @@ void setup() {
     // noiseShortFreq ?
     "sbrs r3, 4 \n"
     "rjmp LONGFREQ \n"
-    // noiseReg >> 6  下位 1 bit があれば良い -> r17に 1 bitだけ
+    // noiseReg >> 6. Only least significant bit is needed -> write 1 bit on r17
     // [r17 (noiseReg >> 6) & 1, r19:r18 noiseReg]
     "clr r17 \n"
-    "sbrc r18, 6 \n" // 6bit目が0ならスキップ，1ならスキップせず1
+    "sbrc r18, 6 \n" // If 6th bit is 0, then skip. 1 then don't skip and r17 is 1.
     "ldi r17, 1 \n"
     "rjmp DONE \n"
     // noiseReg >> 1
@@ -170,8 +176,8 @@ void setup() {
     "clr r16 \n"
     "lsr r17 \n"
     "ror r16 \n"
-    // [r16 15shifted, r19:r18 noiseReg]
-    // 最上位1bitだけ 0 or 1，残りは << 15 の為 0 -> 結果は r17
+    // [r16 15-shifted, r19:r18 noiseReg]
+    // Only the most significant bit is to 0 or 1. The rest is 0 because of "<< 15". The result is r17
     // noiseReg r19 |= r16
     "or r19, r16 \n"
     "sts noiseReg, r18 \n"
@@ -225,7 +231,7 @@ void setup() {
     // [r16 sq1Pointer, r5 sq1Duty]
   "SQ1_ENABLE:"
     // ((pointer & 0b111) <= (duty == 0 ? duty << 1 : duty << 1 - 1)) ? 1 : 0
-    "andi r16, 0b111 \n" // pointer の下位3バイト
+    "andi r16, 0b111 \n" // pointer's lowest 3 bit
     "lds r5, sq1Duty \n"
     "lsl r5 \n"
     "cpse r5, r1 \n"
@@ -267,7 +273,7 @@ void setup() {
     // [r16 sq2Pointer, r5 sq2Duty]
   "SQ2_ENABLE:"
     // ((pointer & 0b111) <= (duty == 0 ? duty << 1 : duty << 1 - 1)) ? 1 : 0
-    "andi r16, 0b111 \n" // pointer の下位3バイト
+    "andi r16, 0b111 \n" // pointer's lowest 3 bit
     "lds r5, sq2Duty \n"
     "lsl r5 \n"
     "cpse r5, r1 \n"
